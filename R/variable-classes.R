72b5cedfd07e1987eb148272cf9869f63526570d
Jan Kückelhaus <jankueckelhaus@gmx.de>
2020-08-03 19:34:07 +0200

    variable_classes2

--- R/variable-classes.R
+++ R/variable-classes.R
@@ -27,3 +27,35 @@ variable_classes <- function(data){
   base::return(dnames)
 
 }
+
+
+
+#' @title Classes with names
+#'
+#' @description Get information about the inputs
+#' variable names and classes.
+#'
+#' @param data A data.frame or a list.
+#'
+#' @return  A named character vector.
+#' \itemize{
+#'  \item{ \strong{Vector-elements:} The class of every variable of \code{data}}.
+#'  \item{ \strong{Vector-names:} The name of every variable of \code{data}}.
+#'  }
+#'
+#' @export
+#'
+#' @examples variable_classes(data = mtcars)
+#'
+
+variable_classes2 <- function(data){
+
+  stopifnot(base::is.list(data) | base::is.data.frame(data))
+
+  dclasses <- base::sapply(data, base::class)
+
+  base::names(dclasses) <- base::names(data)
+
+  base::return(dclasses)
+
+}
