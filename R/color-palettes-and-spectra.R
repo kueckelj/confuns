f00c356bbea1161d063927534920ffcaae90713b
Jan Kückelhaus <jankueckelhaus@gmx.de>
2020-08-21 14:12:54 +0200

    clprs

---
+++ R/color-palettes-and-spectra.R
@@ -0,0 +1,46 @@
+#' @title Color palette names
+#' @export
+
+sequential_single_hue <-
+  c("Grays", "Light Grays", "Blues 2", "Blues 3", "Purples 2", "Purples 3", "Reds 2", "Reds 3", "Greens 2,
+    Greens 3", "Oslo")
+
+#' @rdname sequential_single_hue
+#' @export
+sequential_multi_hue <-
+  c("Purple-Blue", "Red-Purple", "Red-Blue", "Purple-Orange", "Purple-Yellow", "Blue-Yellow,
+  Green-Yellow", "Red-Yellow", "Heat", "Heat 2", "Terrain", "Terrain 2", "Viridis", "Plasma,
+  Inferno", "Dark Mint", "Mint", "BluGrn", "Teal", "TealGrn", "Emrld", "BluYl", "ag_GrnYl", "Peach,
+  PinkYl", "Burg", "BurgYl", "RedOr", "OrYel", "Purp", "PurpOr", "Sunset", "Magenta", "SunsetDark,
+  ag_Sunset", "BrwnYl", "YlOrRd", "YlOrBr", "OrRd", "Oranges", "YlGn", "YlGnBu", "Reds", "RdPu", "PuRd,
+  Purples", "PuBuGn", "PuBu", "Greens", "BuGn", "GnBu", "BuPu", "Blues", "Lajolla", "Turku")
+
+#' @rdname sequential_single_hue
+#' @export
+diverging <-
+  c("Blue-Red", "Blue-Red 2", "Blue-Red 3", "Red-Green", "Purple-Green", "Purple-Brown", "Green-Brown,
+    Blue-Yellow 2", "Blue-Yellow 3", "Green-Orange", "Cyan-Magenta", "Tropic", "Broc", "Cork,
+    Vik", "Berlin", "Lisbon", "Tofino")
+
+
+
+#' @title Color panels
+#' @export
+
+milo_clrp <- c("#C4432A", "#3A389C", "#478C3D", "#FFD700", "steelblue", "#FFA500", "#800000FF", "#9E344D",  "#8B2252", "#56D9ED", "#C934BD",
+                 "#64DB74", "#C9B972", "#4F1211", "#CD4F39", "#00868B", "#8B7355",
+                 "#CAFF70", "#2C6CA3", "#525252", "#EEAEEE", "#F7E70A", "#2E4AE8", "#E3E61E", "#058C00")
+
+#'  @rdname milo_clrp
+#'  @export
+
+ggsci_list <- list(
+  "Journal of Clinical Oncology" = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF", "#7AA6DCFF", "#003C67FF", "#8F7700FF", "#3B3B3BFF", "#A73030FF", "#4A6990FF"),
+  "Nature Publishing Group" = c("#E64B35FF", "#4DBBD5FF", "#00A087FF", "#3C5488FF", "#F39B7FFF", "#8491B4FF", "#91D1C2FF", "#DC0000FF", "#7E6148FF", "#B09C85FF"),
+  "American Association for the Advancement of Science" = c("#3B4992FF", "#EE0000FF", "#008B45FF", "#631879FF", "#008280FF", "#BB0021FF", "#5F559BFF", "#A20056FF", "#808180FF", "#1B1919FF"),
+  "The New England Journal of Medicine" = c("#BC3C29FF", "#0072B5FF", "#E18727FF", "#20854EFF", "#7876B1FF", "#6F99ADFF", "#FFDC91FF", "#EE4C97FF","#b15928","#ffff33"),
+  "Lancet Oncology" = c("#00468BFF", "#ED0000FF", "#42B540FF", "#0099B4FF", "#925E9FFF", "#FDAF91FF", "#AD002AFF", "#ADB6B6FF", "#1B1919FF", "#800000FF"),
+  "Journal of the American Medical Association" = c("#374E55FF", "#DF8F44FF", "#00A1D5FF", "#B24745FF", "#79AF97FF", "#6A6599FF", "#80796BFF", "#008B45FF", "#fdbf6f", "#377eb8"),
+  "University of Chicago" = c("#800000FF", "#767676FF", "#FFA319FF", "#8A9045FF", "#155F83FF", "#C16622FF", "#8F3931FF", "#58593FFF", "#350E20FF", "#1F77B4FF")
+)
+
