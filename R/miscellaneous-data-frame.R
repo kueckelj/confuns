39543b910dada049176f9d2145ec88894aed9553
Jan Kückelhaus <jankueckelhaus@gmx.de>
2021-01-16 17:30:23 +0100

    relevel control

--- R/miscellaneous-data-frame.R
+++ R/miscellaneous-data-frame.R
@@ -1,20 +1,22 @@
 #' Subset the across-variables
 #'
 #' @description Checks across and across.subset input and if at least one
-#' of the across.subset values exists filters the data.frame accordingly.
+#' of the \code{across.subset} values exists if the letter is not NULL. It
+#' then filters the input data.frame accordingly.
 #'
-#' @param df A data.frame that contains the variable specified in \code{across}.
+#' @param df A data.frame that contains the grouping variable specified in \code{across}.
 #' @param across Character value. Denotes the discrete variable in the data.frame
-#' across which the variables of interest are to be analyzed or displayed. Valid input
-#' options can be obtained via \code{getAcrossOptions()}.
-#' @param across.subset Character vector. The groups of interest that the \code{across}-
-#' variable contains..
+#' across which the variables of interest are to be analyzed or displayed.
+#' @param across.subset Character vector. The groups of interest that the grouping variable
+#' denoted in \code{across} contains.
+#' @param relevel Logical value. If set to TRUE the input of \code{across.subset}
+#' determines the new order in which the results are displayed.
 #'
 #' @return A filtered data.frame, informative messages or an error.
 #' @export
 #'
 
-check_across_subset <- function(df, across, across.subset, fdb.fn = "warning"){
+check_across_subset <- function(df, across, across.subset, relevel = TRUE, fdb.fn = "warning"){
 
   if(base::is.null(across.subset)){
 
@@ -42,7 +44,7 @@ check_across_subset <- function(df, across, across.subset, fdb.fn = "warning"){
 
     df <- dplyr::filter(.data = df, !!rlang::sym(across) %in% {{across.subset}})
 
-    if(base::is.factor(df[[across]])){
+    if(base::is.factor(df[[across]]) & base::isTRUE(relevel)){
 
       df[[across]] <-
         base::factor(x = df[[across]], levels = across.subset)
