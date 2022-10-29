d721c68e949ac18f74095fde31aa0f52750a9b49
Jan Kückelhaus <jankueckelhaus@gmx.de>
2022-10-29 23:31:07 +0200

    select helper update

--- R/tidy-select-leverage.R
+++ R/tidy-select-leverage.R
@@ -8,7 +8,10 @@
 #' @param input A character vector or a factor.
 #' @param lst A named list. (Unnamed elements are discarded.)
 #' @param ... Additional selection helpers from the \code{tidyselect} package that match
-#' variable names according to a given pattern.
+#' names according to a given pattern.
+#' @param out.fail Output if no content remains after tidyselection has been tried.
+#'
+#' @note \code{vselect()} selects by element. \code{lselect()} selects by element name.
 #'
 #' @return A subsetted version of the input.
 #'
@@ -16,7 +19,22 @@
 #'
 #' @export
 #'
-vselect <- function(input, ...){
+#' @examples
+#'
+#'  library(datasets)
+#'  library(confuns)
+#'
+#'  vec <- c("apple", "banana", "pineapple", "tomato")
+#'
+#'  vselect(vec, contains("apple") | ends_with("o"))
+#'
+#'  lst <- list(a = mtcars, ab = iris, ac = cars)
+#'
+#'  lselect(lst, starts_with("a") & !ends_with("c"))
+#'
+#'
+#'
+vselect <- function(input, ..., out.fail = base::character(0)){
 
   original_input <- input
 
@@ -64,18 +82,21 @@ vselect <- function(input, ...){
 
     if(base::isTRUE(selection_helpers_provided)){
 
-      base::stop("Tidyselect input resulted in no output.")
+      output <- out.fail
 
       # if FALSE then ncol == 0 because no tidyselection specified: return all variable names
     } else {
 
-      selected_df <- input_df
+      output <- base::colnames(input_df)
 
     }
 
+  } else {
+
+    output <- base::colnames(selected_df)
+
   }
 
-  output <- base::colnames(selected_df)
 
   if("factor" %in% original_class){
 
@@ -84,13 +105,13 @@ vselect <- function(input, ...){
   }
 
 
-  base::return(output)
+  return(output)
 
 }
 
 #' @rdname vselect
 #' @export
-lselect <- function(lst, ...){
+lselect <- function(lst, ..., out.fail = base::list()){
 
   lst <- keep_named(input = lst)
 
@@ -113,7 +134,7 @@ lselect <- function(lst, ...){
     selection_helpers_provided <-
       base::tryCatch({
 
-        # leads to error if tidyselection specified
+        # leads to error if tidy selection specified
         list(...)
 
       }, error = function(error){
@@ -124,98 +145,29 @@ lselect <- function(lst, ...){
 
     if(base::isTRUE(selection_helpers_provided)){
 
-      base::stop("Tidyselect input resulted in no output.")
+      lst_output <- out.fail
 
-      # if FALSE then ncol == 0 because no tidyselection specified: return all variable names
+      # if FALSE then ncol == 0 because no tidy selection specified: return all variable names
     } else {
 
-      selected_df <- input_df
+      output <- base::colnames(input_df)
+
+      lst_output <- lst[output]
 
     }
 
-  }
+  } else {
 
-  output <- base::colnames(selected_df)
+    output <- base::colnames(selected_df)
 
-
-  lst_output <- lst[output]
-
-  base::return(lst_output)
-
-}
-
-#' @rdname vselect
-#' @export
-vector_select <- function(input, ...){
-
-  warning("vector_select() is deprecated in favor of vselect()")
-
-  original_input <- input
-
-  original_class <- base::class(input)
-
-  if(!base::is.data.frame(input)){
-
-    if(base::is.character(input)){
-
-      input <- base::unique(input)
-
-      n_cols <- base::length(input)
-
-    } else if(base::is.factor(input)){
-
-      input <- base::levels(input)
-
-      n_cols <- base::length(input)
-
-    }
-
-    input_df <-
-      base::matrix(data = 1, ncol = n_cols) %>%
-      base::as.data.frame() %>%
-      magrittr::set_colnames(value = input)
-
-  }
-
-  selected_df <- dplyr::select(input_df, ...)
-
-  if(base::ncol(selected_df) == 0){
-
-    # if TRUE then ncol == 0 because selection resulted in no vars
-    selection_helpers_provided <-
-      base::tryCatch({
-
-        # leads to error if tidyselection specified
-        list(...)
-
-      }, error = function(error){
-
-        TRUE
-
-      })
-
-    if(base::isTRUE(selection_helpers_provided)){
-
-      base::stop("Tidyselect input resulted in no output.")
-
-      # if FALSE then ncol == 0 because no tidyselection specified: return all variable names
-    } else {
-
-      selected_df <- input_df
-
-    }
-
-  }
-
-  output <- base::colnames(selected_df)
-
-  if("factor" %in% original_class){
-
-    output <- base::as.factor(output)
+    lst_output <- lst[output]
 
   }
 
 
-  base::return(output)
+
+
+
+  return(lst_output)
 
 }
